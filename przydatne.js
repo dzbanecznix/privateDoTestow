db440562229b7eda2cdbf500d359fa946a000aac
dzbanecznix <60468598+dzbanecznix@users.noreply.github.com>
2020-10-01 17:23:26 +0200

    Update przydatne.js

--- przydatne.js
+++ przydatne.js
@@ -1,9 +1,10 @@
-async function getCode(url){
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto('https://raw.githubusercontent.com/dzbanecznix/privateDoTestow/master/h');
-const pg = await page.content();
-srccode = pg.substr(84, pg.length-104);
-await browser.close();
-var parser = new DOMParser().parseFromString(srccode, "text/html");
-return parser.documentElement.textContent;}
+async function getCode(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://raw.githubusercontent.com/dzbanecznix/privateDoTestow/master/h');
+  const pg = await page.content();
+  srccode = pg.substr(84, pg.length - 104);
+  await browser.close();
+  var parser = new DOMParser().parseFromString(srccode, "text/html");
+  return parser.documentElement.textContent;
+}
