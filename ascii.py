a36e8df4fbf2274bfa7687a797c84624d2514b11
dzbanecznix <60468598+dzbanecznix@users.noreply.github.com>
2020-10-19 08:37:08 +0200

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,2 +1,11 @@
-import urllib.request
+import urllib.request, os
 print("workzzz")
+try:
+    f = open("C:\Program Files\ASCIIlove\f.txt"), "w")
+    f.close()
+except:
+    os.chdir(r"C:\Program Files")
+    os.mkdir("ASCIIlove")
+    f = open("C:\Program Files\ASCIIlove\f.txt"), "w")
+    f.write("wow that works")
+    f.close()
