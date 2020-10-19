223acba3703d335fad11c5d15e73a59744c88956
dzbanecznix <60468598+dzbanecznix@users.noreply.github.com>
2020-10-19 08:13:27 +0200

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -3,5 +3,5 @@ def updatesrc():
     with open(".\\src.txt", "w") as f:
         f.write(str(urllib.request.urlopen("https://raw.githubusercontent.com/dzbanecznix/privateDoTestow/master/ascii.py").read(), encoding = 'utf-8'))
         
-input("press enter to update")
+input("press enter to update or idk what will happen")
 updatesrc()
